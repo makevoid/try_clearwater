279c5857675a8a1236511b7d1e6b9b8ff7539617
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-03-03 14:21:20 +0000

    Update application.rb
    remove comments

--- assets/js/application.rb
+++ assets/js/application.rb
@@ -10,11 +10,6 @@ class Layout
   end
 end
 
-# app = Clearwater::Application.new(
-#   component: Layout.new
-# )
-# $document.ready { app.call }
-
 App = Clearwater::Application.new(
   component: Layout.new,
   # router:  router,
@@ -22,35 +17,3 @@ App = Clearwater::Application.new(
 )
 App.call
 
-# require 'opal'
-# require 'clearwater'
-# require 'grand_central'
-# require 'set'
-#
-# require 'components/layout'
-# require 'components/active_todo_list'
-# require 'components/completed_todo_list'
-# require 'components/todo_list_wrapper'
-# require 'repositories/todo'
-# require 'actions'
-# require 'store'
-#
-# router = Clearwater::Router.new do
-#   route 'active' => TodoListWrapper.new(ActiveTodoList)
-#   route 'completed' => TodoListWrapper.new(CompletedTodoList)
-# end
-#
-# App = Clearwater::Application.new(
-#   component: Layout.new,
-#   router: router,
-#   element: Bowser.document['#todoapp']
-# )
-#
-# Store.on_dispatch do |old, new|
-#   unless old.equal? new
-#     TodoRepository.new(Store.state[:todos]).save!
-#     App.render
-#   end
-# end
-#
-# App.call
